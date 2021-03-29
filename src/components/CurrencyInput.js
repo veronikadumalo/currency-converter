9c82f01f6010d07912cc19deb6be7866b1faf613
veronikadumalo <dumalo44084@gmail.com>
2021-03-29 18:50:48 +0200

    added two select elements

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function CurrencyInput() {
     return (
         <div>
-            <input type="number" />
+            <input type="number" onChange=""/>
+            <select>
+                <option value="1">1</option>
+            </select>
         </div>
     )
 }

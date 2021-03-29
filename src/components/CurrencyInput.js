9571ca25bcd42ca0a093907c372b43a0c2994b95
veronikadumalo <dumalo44084@gmail.com>
2021-03-29 21:33:53 +0200

    changed selected value

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 export default function CurrencyInput(props) {
-    const {currencyName} = props
+    const {
+        currencyName,
+        selectedCurrency
+    } = props
     return (
         <div>
             <input type="number" />
             <select>
                 {currencyName && currencyName.map(option =>(
-                    <option key={option} value={option}>{option}</option>
+                    <option key={option} value={selectedCurrency}>{option}</option>
                 ))}
             </select>
         </div>

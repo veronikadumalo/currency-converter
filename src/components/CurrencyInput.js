74a36266e3271334dde537236010b69b253b4448
veronikadumalo <dumalo44084@gmail.com>
2021-03-29 20:20:47 +0200

    created currencyName props in input file

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export default function CurrencyInput() {
+export default function CurrencyInput(props) {
+    const {currencyName} = props;
     return (
         <div>
             <input type="number" />
             <select>
+                {currencyName.map}
                 <option value="1">1</option>
             </select>
         </div>

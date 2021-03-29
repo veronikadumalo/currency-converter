151bd486cf9e0536db3f8dcfb4064f2540a8c85c
veronikadumalo <dumalo44084@gmail.com>
2021-03-29 19:57:04 +0200

    used useState hook: declared a state variable currency and function setCurrency. Added to currency state names of currency from api

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,18 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import CurrencyInput from './CurrencyInput'
 
 const currencyUrl = 'https://api.exchangeratesapi.io/latest';
 
 export default function Calculator() {
+    const [currency, setCurrency] = useState();
+    console.log(currency);
     useEffect(() => {
         fetch(currencyUrl)
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>
+            {
+                setCurrency([data.base, ...Object.keys(data.rates)])
+            })
     }, [])
     return (
         <div>

8ce3d358d6f1cf8242f9675248ed43b1ea962454
veronikadumalo <dumalo44084@gmail.com>
2021-03-29 18:47:00 +0200

    added two number input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import CurrencyInput from './components/CurrencyInput'
 
 function App() {
   return (
     <div className="App">
-      
+      <CurrencyInput />
+      <CurrencyInput />
     </div>
   );
 }
--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CurrencyInput() {
     return (
         <div>
-            
+            <input type="number" />
         </div>
     )
 }
